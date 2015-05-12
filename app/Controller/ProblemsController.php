3492a58a94e90ee69d789c8277a00557248fc209
Hiroyuki Inoue <omega999kurahuto@yahoo.co.jp>
2015-05-12 23:26:18 +0900

    modify answerMethod,セッション書き込み処理の追加とデバック用処理の削除

--- app/Controller/ProblemsController.php
+++ app/Controller/ProblemsController.php
@@ -43,6 +43,7 @@ class ProblemsController extends AppController {
         //回答画面
         $querys = 'kentei_id='.MORIKEN_WEB_ID.'&'.'employ='.$employ.'&'.'public_flag='.$publicFlag.'&'.'category_id='.$categoryId.'&'.'grade='.$grade.'&'.'item='.$item;
         $response = $this->api_rest('GET', 'problems/index.json', $querys, array());
+        $this->Session->write('problems', $response['response']['Problems']);
         $extractNames = array(
             'sentence','right_answer','wrong_answer1','wrong_answer2','wrong_answer3','description','other_answer'
         );
@@ -54,7 +55,6 @@ class ProblemsController extends AppController {
         }else{
             $this->render('answer_list');
         }
-        $this->render('answer_solo');
     }
 
     public function result() {
